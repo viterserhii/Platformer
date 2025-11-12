08b0d455d55ecc527dd350bab9d78348bd09ae2c
Viter Serhii <vitersergii@gmail.com>
2025-11-12 13:53:00 +0200

    feat: added new checkpoint

--- Source/Platformer/Private/Checkpoint.cpp
+++ Source/Platformer/Private/Checkpoint.cpp
@@ -1,20 +1,63 @@
 #include "Checkpoint.h"
+#include "Components/SphereComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "MyGameMode.h"
 
 ACheckpoint::ACheckpoint()
 {
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = false;
 
+    Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+    SetRootComponent(Collision);
+    Collision->InitSphereRadius(80.f);
+    Collision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    Collision->SetCollisionResponseToAllChannels(ECR_Ignore);
+    Collision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+    Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+    Arrow->SetupAttachment(RootComponent);
+
+    BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+    BaseMesh->SetupAttachment(RootComponent);
+    BaseMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    ButtonMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ButtonMesh"));
+    ButtonMesh->SetupAttachment(RootComponent);
+    ButtonMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    Collision->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnBeginOverlap);
 }
 
 void ACheckpoint::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
-void ACheckpoint::Tick(float DeltaTime)
+void ACheckpoint::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
+    if (bActivated) return;
+    if (!OtherActor || !OtherActor->IsA<ACharacter>()) return;
 
+    if (AMyGameMode* GM = GetWorld()->GetAuthGameMode<AMyGameMode>())
+    {
+        GM->UpdateSpawnPoint(Arrow->GetComponentLocation(), Arrow->GetComponentRotation());
+        bActivated = true;
+
+        if (ActivateSound)
+        {
+            UGameplayStatics::PlaySoundAtLocation(this, ActivateSound, GetActorLocation());
+        }
+
+        if (ButtonActiveMaterial && ButtonMesh)
+        {
+            ButtonMesh->SetMaterial(0, ButtonActiveMaterial);
+        }
+    }
 }
+
 
--- Source/Platformer/Public/Checkpoint.h
+++ Source/Platformer/Public/Checkpoint.h
@@ -4,18 +4,46 @@
 #include "GameFramework/Actor.h"
 #include "Checkpoint.generated.h"
 
+class USphereComponent;
+class UStaticMeshComponent;
+class UArrowComponent;
+class USoundBase;
+class UMaterialInterface;
+
 UCLASS()
 class PLATFORMER_API ACheckpoint : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	ACheckpoint();
+    GENERATED_BODY()
+
+public:
+    ACheckpoint();
 
 protected:
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-public:	
-	virtual void Tick(float DeltaTime) override;
+private:
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    USphereComponent* Collision = nullptr;
 
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UArrowComponent* Arrow = nullptr;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UStaticMeshComponent* BaseMesh = nullptr;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UStaticMeshComponent* ButtonMesh = nullptr;
+
+    UPROPERTY(EditAnywhere, Category = "Checkpoint")
+    UMaterialInterface* ButtonActiveMaterial = nullptr;
+
+    UPROPERTY(EditAnywhere, Category = "Checkpoint")
+    USoundBase* ActivateSound = nullptr;
+
+    bool bActivated = false;
+
+    UFUNCTION()
+    void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& SweepResult);
 };
